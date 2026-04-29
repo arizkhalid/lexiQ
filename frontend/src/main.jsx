374f3a0a9c671ff8b41c6ee9db8b1b0e33a71e68
i250882-os <i250882@isb.nu.edu.pk>
2026-04-29 22:20:25 +0500

    BugFix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ScrollTop from "./components/ScrollTop";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollTop />
       <App />
     </BrowserRouter>
   </StrictMode>,
