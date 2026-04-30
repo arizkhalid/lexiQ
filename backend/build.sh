0f23bf994faaed2a34c784d8abbd1a9d4b8fbf28
i250882-os <i250882@isb.nu.edu.pk>
2026-04-30 23:33:54 +0500

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -4,4 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py loaddata db.json
+python manage.py loaddata seed_data.json
